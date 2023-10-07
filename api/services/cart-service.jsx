00d03485de32714fca2877702919ebec8616563f
Ammar M. Kamel <ammarkamel9899@gmail.com>
2023-10-07 09:13:16 +0300

    PX-56 Add cart api services

---
+++ api/services/cart-service.jsx
@@ -0,0 +1,67 @@
+// Importing the pre-configured API instance and its associated error handling function
+import { apiInstance, apiErrorHandler } from "../config/api-config";
+
+// Setting up the endpoint for the API to be the product endpoint in the environment variables
+const endpoint = process.env.REACT_APP_CART_ENDPOINT;
+
+/**
+ * Retrieves all carts from the API's endpoint
+ * @async
+ * @function getAllCarts
+ * @throws {Error} When the API call fails
+ * @returns {Promise<object>} The data from the API's response
+ */
+
+export const getAllCarts = async () => {
+	try {
+		const response = await apiInstance.get(endpoint);
+
+		return response.data;
+	} catch (error) {
+		const msg = "There was a problem getting all carts";
+		apiErrorHandler(error, msg);
+	}
+};
+
+/**
+ * Retrieves a specific cart from the API's endpoint using its ID
+ * @async
+ * @function getSingleCart
+ * @param {string} cartId - The ID of the specific cart to retrieve
+ * @throws {Error} When the API call fails
+ * @returns {Promise<object>} The data from the API's response
+ */
+
+export const getSingleCart = async (cartId) => {
+	try {
+		const url = endpoint + cartId;
+		const response = await apiInstance.get(url);
+
+		return response.data;
+	} catch (error) {
+		const msg = "There was a problem getting the specified cart";
+		apiErrorHandler(error, msg);
+	}
+};
+
+/**
+ * Retrieves all carts associated with a specific user from the API's endpoint using their ID
+ * @async
+ * @function getUserCarts
+ * @param {string} userId - The ID of the specific user for which to retrieve carts
+ * @throws {Error} When the API call fails
+ * @returns {Promise<object>} The data from the API's response
+ */
+
+export const getUserCarts = async (userId) => {
+	try {
+		const url = endpoint + `user/${userId}`;
+		const response = await apiInstance.get(url);
+
+		return response.data;
+	} catch (error) {
+		const msg =
+			"There was a problem getting the carts of the specified user";
+		apiErrorHandler(error, msg);
+	}
+};
