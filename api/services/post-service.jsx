d6ba95669c567b83134fd5c0017ea2e904dcc803
Ammar M. Kamel <ammarkamel9899@gmail.com>
2023-10-07 09:13:54 +0300

    PX-56 Add post api services

---
+++ api/services/post-service.jsx
@@ -0,0 +1,57 @@
+// Importing the pre-configured API instance and its associated error handling function
+import { apiInstance, apiErrorHandler } from "../config/api-config";
+
+// Setting up the endpoint for the API to be the product endpoint in the environment variables
+const endpoint = process.env.REACT_APP_POST_ENDPOINT;
+
+/**
+ * Fetch all posts from the API
+ *
+ * @returns {Promise} A promise that resolves to the data of the API response
+ * @throws {Error} If there is a problem retrieving the posts
+ */
+export const getAllPosts = async () => {
+	try {
+		const response = await apiInstance.get(endpoint);
+		return response.data;
+	} catch (error) {
+		const msg = "There was a problem getting all posts";
+		apiErrorHandler(error, msg);
+	}
+};
+
+/**
+ * Fetch a single post from the API
+ *
+ * @param {String} postId The ID of the post to retrieve
+ * @returns {Promise} A promise that resolves to the data of the API response
+ * @throws {Error} If there is a problem retrieving the post
+ */
+export const getSinglePost = async (postId) => {
+	try {
+		const url = endpoint + postId;
+		const response = await apiInstance.get(url);
+		return response.data;
+	} catch (error) {
+		const msg = "There was a problem getting the specified post";
+		apiErrorHandler(error, msg);
+	}
+};
+
+/**
+ * Search for posts from the API using a search term
+ *
+ * @param {String} searchTerm The term to use in searching for posts
+ * @returns {Promise} A promise that resolves to the data of the API response
+ * @throws {Error} If there is a problem searching for the posts
+ */
+export const getPostsBySearch = async (searchTerm) => {
+	try {
+		const url = endpoint + `search?q=${searchTerm}`;
+		const response = await apiInstance.get(url);
+		return response.data;
+	} catch (error) {
+		const msg = "There was a problem searching for " + searchTerm;
+		apiErrorHandler(error, msg);
+	}
+};
