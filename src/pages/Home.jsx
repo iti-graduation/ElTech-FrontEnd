aa4f25127d901a2d896b180ae89d76d4e120ad9a
Ahmed Mokhtar <35121487+00AhmedMokhtar00@users.noreply.github.com>
2023-10-13 15:40:10 +0300

    PX-94 slider done/ js files moved to public

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Clients from "../layouts/Clients";
 import Services from "../layouts/Services";
+import Slider from "../layouts/Slider";
 
 export default function Home() {
 	return (
 		<>
+			<Slider />
 			<Services />
             <Clients />
 		</>
