554229d52d97404a2f50d6bc8dbe42144553e0b0
Aalaa <aalaabadr268@gmail.com>
2023-10-15 17:49:50 +0300

    PX-98 weekly section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Preloader from "../layouts/Home/Preloader";
 import Header from "../layouts/Home/Header";
 import PopUpSearch from "../layouts/Home/PopUpSearch";
 import Trending from "../layouts/Home/Trending";
+import WeeklyDeal from "../layouts/Home/WeeklyDeal";
 import News from "../layouts/Home/News";
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
       <Slider />
       <Services />
       <Trending />
+      <WeeklyDeal />
       <News />
       <Clients />
     </>
