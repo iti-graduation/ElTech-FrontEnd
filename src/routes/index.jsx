b2a015a494c44ef04b36b8e30112f71bb39540db
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2023-10-21 05:40:53 +0300

    PX-107 Create the route for the checkout page on the main page navbar

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,22 +1,23 @@
-
-import React, { Suspense } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from '../pages/Home'
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Checkout from "../pages/Checkout";
 
 // import pages
 // const PageName = React.lazy(() => import('path'));
 
 export default function Router() {
-    return (
-      <Suspense
-        fallback={
-          // add loading element
-          <></>
-        }
-      >
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Suspense>
-	  );
+  return (
+    <Suspense
+      fallback={
+        // add loading element
+        <></>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </Suspense>
+  );
 }
