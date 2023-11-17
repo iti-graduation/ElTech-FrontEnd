346eaa96b9e116cfe5107231e0091716126fd8f7
Ammar M. Kamel <ammarkamel9899@gmail.com>
2023-11-18 01:56:53 +0200

    Implement subscribe and unsubscribe in user profile

--- src/components/UserAccount/UserProfile/UserDetails.jsx
+++ src/components/UserAccount/UserProfile/UserDetails.jsx
@@ -1,47 +1,127 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { formatPhoneNumber } from '../../../utils/helpers';
+import React from "react";
+import { useSelector } from "react-redux";
+import { formatPhoneNumber } from "../../../utils/helpers";
 
-export default function UserCard({onEdit, onLogout}) {
-  const user = useSelector((state) => state.authSlice.user);
+import {
+	subscribe,
+	unsubscribe,
+	getUserData,
+} from "../../../api/services/user/user-services";
 
-  return (
-    <div className="card-body" id="userCardBody">
+import { showToast } from "../../../utils/toastUtil";
 
-    <div className="row">
-      <div className="col-sm-3">
-        <h6 className="mb-0">Full Name</h6>
-      </div>
-      <div className="col-sm-9 text-secondary">{user? user.first_name + " " + user.last_name : "John Doe"}</div>
-    </div>
-    <hr />
-    <div className="row">
-      <div className="col-sm-3">
-        <h6 className="mb-0">Email</h6>
-      </div>
-      <div className="col-sm-9 text-secondary">{user? user.email : "JohnDoe@mail.com"}</div>
-    </div>
-    <hr />
-    <div className="row">
-      <div className="col-sm-3">
-        <h6 className="mb-0">Phone</h6>
-      </div>
-      <div className="col-sm-9 text-secondary">{user? formatPhoneNumber(user.mobile_phone) : "(239) 816-9029"}</div>
-    </div>
-    <hr />
-    {user && user.birth_date &&
-    <>
-      <div className="row">
-        <div className="col-sm-3">
-          <h6 className="mb-0">BirthDay</h6>
-        </div>
-        <div className="col-sm-9 text-secondary">{user? user.birth_date : "01-01-2001"}</div>
-      </div>
-      <hr />
-    </>}
-    
+export default function UserCard({ onEdit, onLogout }) {
+	const user = useSelector((state) => state.authSlice.user);
 
-    {/* <div className="row">
+	const handleSubscribe = async (event) => {
+		event.preventDefault();
+		try {
+			await subscribe(user.email);
+			await getUserData(); // refresh user data
+			showToast("You have subscribed successfully!", "success"); // show success toast
+			window.location.reload(); // reload the page
+		} catch (error) {
+			showToast(
+				"There was a problem subscribing. Please try again.",
+				"error"
+			); // show error toast
+			console.error(error);
+		}
+	};
+
+	const handleUnsubscribe = async (event) => {
+		event.preventDefault();
+		try {
+			await unsubscribe(user.email);
+			await getUserData(); // refresh user data
+			showToast("You have unsubscribed successfully!", "success"); // show success toast
+			window.location.reload(); // reload the page
+		} catch (error) {
+			showToast(
+				"There was a problem unsubscribing. Please try again.",
+				"error"
+			); // show error toast
+			console.error(error);
+		}
+	};
+
+	return (
+		<div className="card-body" id="userCardBody">
+			<div className="row">
+				<div className="col-sm-3">
+					<h6 className="mb-0">Full Name</h6>
+				</div>
+				<div className="col-sm-9 text-secondary">
+					{user ? user.first_name + " " + user.last_name : "John Doe"}
+				</div>
+			</div>
+			<hr />
+			<div className="row">
+				<div className="col-sm-3">
+					<h6 className="mb-0">Email</h6>
+				</div>
+				<div className="col-sm-9 text-secondary">
+					{user ? user.email : "JohnDoe@mail.com"}
+				</div>
+			</div>
+			<hr />
+			<div className="row">
+				<div className="col-sm-3">
+					<h6 className="mb-0">Phone</h6>
+				</div>
+				<div className="col-sm-9 text-secondary">
+					{user
+						? formatPhoneNumber(user.mobile_phone)
+						: "(239) 816-9029"}
+				</div>
+			</div>
+			<hr />
+			<div className="row">
+				<div className="col-sm-3 d-flex align-items-center">
+					<h6 className="mb-0">Newsletter</h6>
+				</div>
+				<div className="col-sm-9 text-secondary d-flex justify-content-between align-items-center">
+					{user && user.is_subscribed
+						? "Subscribed"
+						: "Not Subscribed"}
+					{user && user.is_subscribed && (
+						<form
+							className="mailchimp-form"
+							onSubmit={handleUnsubscribe}
+						>
+							<button className="" type="submit">
+								Unsubscribe
+							</button>
+						</form>
+					)}
+					{user && !user.is_subscribed && (
+						<form
+							className="mailchimp-form"
+							onSubmit={handleSubscribe}
+						>
+							<button className="" type="submit">
+								Subscribe
+							</button>
+						</form>
+					)}
+				</div>
+			</div>
+			<hr />
+			{user && user.birth_date && (
+				<>
+					<div className="row">
+						<div className="col-sm-3">
+							<h6 className="mb-0">BirthDay</h6>
+						</div>
+						<div className="col-sm-9 text-secondary">
+							{user ? user.birth_date : "01-01-2001"}
+						</div>
+					</div>
+					<hr />
+				</>
+			)}
+
+			{/* <div className="row">
       <div className="col-sm-3">
         <h6 className="mb-0">Address</h6>
       </div>
@@ -50,10 +130,14 @@ export default function UserCard({onEdit, onLogout}) {
       </div>
     </div> */}
 
-    {/* <hr /> */}
-        <button id="editbtn" onClick={onEdit}>Edit</button>
+			{/* <hr /> */}
+			<button id="editbtn" onClick={onEdit}>
+				Edit
+			</button>
 
-        <button className="goru-btn-logout" onClick={onLogout}>Logout</button>
-  </div>
-  )
+			<button className="goru-btn-logout" onClick={onLogout}>
+				Logout
+			</button>
+		</div>
+	);
 }
