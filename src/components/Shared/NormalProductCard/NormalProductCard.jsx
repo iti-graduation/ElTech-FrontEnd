d17cf9bb2a6e3d490986d2f217c2961f181b7c66
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2023-11-29 17:36:39 +0200

    added a feedback message for adding a product to favs in the product details page

--- src/components/Shared/NormalProductCard/NormalProductCard.jsx
+++ src/components/Shared/NormalProductCard/NormalProductCard.jsx
@@ -9,78 +9,70 @@ import ProductPrice from "./ProductPrice";
 import ProductAddToCart from "./ProductAddToCart";
 
 const NormalProductCard = ({
-	product,
-	isPopularOrRelated = false,
-	handleAddProductToCart,
-	toggleRefresh,
+  product,
+  isPopularOrRelated = false,
+  handleAddProductToCart,
+  toggleRefresh,
 }) => {
-	const userIdsToNotify = product.users_to_notify.map((user) => user.id);
-	const user = JSON.parse(localStorage.getItem("user"));
-	const userId = user ? user.id : null;
+  const userIdsToNotify = product.users_to_notify.map((user) => user.id);
+  const user = JSON.parse(localStorage.getItem("user"));
+  const userId = user ? user.id : null;
 
-	const handleAddProductToNotifications = async () => {
-		try {
-			await addProductNotification(product.id);
-			showToast(
-				"Product added to notifications successfully!",
-				"success"
-			);
-			// window.location.reload();
-			toggleRefresh();
-		} catch (error) {
-			showToast(
-				"There was a problem adding the product to notifications",
-				"error"
-			);
-			console.error(error);
-		}
-	};
+  const handleAddProductToNotifications = async () => {
+    try {
+      await addProductNotification(product.id);
+      showToast("Product added to notifications successfully!", "success");
+      // window.location.reload();
+      toggleRefresh();
+    } catch (error) {
+      showToast(
+        "There was a problem adding the product to notifications",
+        "error"
+      );
+      console.error(error);
+    }
+  };
 
-	return (
-		<div className={isPopularOrRelated ? "" : "col-lg-3 col-md-6"}>
-			<div
-				className={
-					isPopularOrRelated
-						? "single-popular-product"
-						: "single-shop-product"
-				}
-				style={{ width: "fit-content" }}
-			>
-				<Link to={`/product/${product.id}`}>
-					<div className="sp-thumb">
-						<img
-							src={product.thumbnail.image}
-							// src={product.image}
-							alt=""
-							style={{
-								maxWidth: "370px",
-								maxHeight: "460px",
-								minHeight: "460px",
-							}}
-						/>
-						<ProductCardBadge
-							badgeClass={product.badge}
-							product={product}
-						/>
-						{/* {product.badge != null && (
+  return (
+    <div className={isPopularOrRelated ? "" : "col-lg-3 col-md-6"}>
+      <div
+        className={
+          isPopularOrRelated ? "single-popular-product" : "single-shop-product"
+        }
+        style={{ width: "fit-content" }}
+      >
+        <Link to={`/product/${product.id}`}>
+          <div className="sp-thumb">
+            <img
+              src={product.thumbnail.image}
+              // src={product.image}
+              alt=""
+              style={{
+                maxWidth: "370px",
+                maxHeight: "460px",
+                minHeight: "460px",
+              }}
+            />
+            <ProductCardBadge badgeClass={product.badge} product={product} />
+            {/* {product.badge != null && (
 							
 						)} */}
-					</div>
-				</Link>
-				<div className="sp-details">
-					{/* <h4>{product.name}</h4> */}
-					<h4>
-						{product.name.length > 30
-							? product.name.substring(0, 30) + "..."
-							: product.name}
-					</h4>
-					<div className="product-price clearfix">
-						<ProductPrice
-							// productPriceBefore={product.price}
-							// productPriceAfter={product.discountedPrice}
-							product={product}
-						/>
-						{/* <span className="price">
+          </div>
+        </Link>
+        <div className="sp-details">
+          {/* <h4>{product.name}</h4> */}
+          <h4>
+            {product.name.length > 30
+              ? product.name.substring(0, 30) + "..."
+              : product.name}
+          </h4>
+          <div className="product-price clearfix">
+            <ProductPrice
+              // productPriceBefore={product.price}
+              // productPriceAfter={product.discountedPrice}
+              product={product}
+            />
+            {/* <span className="price">
               {product.discountedPrice != null && <del>
                 <span>
                   <span className="woocommerce-Price-currencySymbol">$</span>
@@ -91,21 +83,19 @@ const NormalProductCard = ({
                 {product.discountedPrice??product.price}
               </span></ins>
             </span> */}
-					</div>
-					<ProductAddToCart
-						productId={product.id}
-						stock={product.stock}
-						handleAddProductToCart={handleAddProductToCart}
-						handleAddProductToNotifications={
-							handleAddProductToNotifications
-						}
-						userIdsToNotify={userIdsToNotify}
-						userId={userId}
-					/>
-				</div>
-			</div>
-		</div>
-	);
+          </div>
+          <ProductAddToCart
+            productId={product.id}
+            stock={product.stock}
+            handleAddProductToCart={handleAddProductToCart}
+            handleAddProductToNotifications={handleAddProductToNotifications}
+            userIdsToNotify={userIdsToNotify}
+            userId={userId}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NormalProductCard;
--- src/components/Shared/NormalProductCard/ProductAddToCart.jsx
+++ src/components/Shared/NormalProductCard/ProductAddToCart.jsx
@@ -3,74 +3,74 @@ import { useSelector } from "react-redux";
 import React, { useState, useEffect } from "react";
 import { apiInstance, FAVORITE_ENDPOINT } from "../../../api/config/api-config";
 import {
-	createUserFavorite,
-	deleteUserFavorite,
+  createUserFavorite,
+  deleteUserFavorite,
 } from "../../../api/services/user/favorite-services";
 
 import { showToast } from "../../../utils/toastUtil";
 
 const ProductAddToCart = ({
-	handleAddProductToNotifications,
-	handleAddProductToCart,
-	productID,
-	productId,
-	stock,
-	userId,
-	userIdsToNotify,
+  handleAddProductToNotifications,
+  handleAddProductToCart,
+  productID,
+  productId,
+  stock,
+  userId,
+  userIdsToNotify,
 }) => {
-	const user = useSelector((state) => state.authSlice.user);
-	const [isFavorite, setIsFavorite] = useState(false);
+  const user = useSelector((state) => state.authSlice.user);
+  const [isFavorite, setIsFavorite] = useState(false);
 
-	const handleAddToWishlist = async (e) => {
-		if (user) {
-			try {
-				if (isFavorite) {
-					// If already favored, remove it
-					// Make a DELETE request to remove the favorite
-					await deleteUserFavorite(productId);
-					setIsFavorite(false);
-					showToast("Product removed from favorites", "success");
-				} else {
-					// If not favored, add it
-					// Make a POST request to create the favorite
-					await createUserFavorite(productId);
-					setIsFavorite(true);
-					showToast("Product added from favorites", "success");
-				}
-			} catch (error) {
-				console.error("Error toggling wishlist:", error.response.data);
-			}
-		} else {
-			// Redirect to the login page
-			window.location.href = "/login";
-		}
-	};
+  const handleAddToWishlist = async (e) => {
+    if (user) {
+      try {
+        if (isFavorite) {
+          // If already favored, remove it
+          // Make a DELETE request to remove the favorite
+          await deleteUserFavorite(productId);
+          setIsFavorite(false);
+          showToast("Product removed from your favorites", "success");
+        } else {
+          // If not favored, add it
+          // Make a POST request to create the favorite
+          await createUserFavorite(productId);
+          setIsFavorite(true);
+          showToast("Product added to your favorites", "success");
+        }
+      } catch (error) {
+        console.error("Error toggling wishlist:", error.response.data);
+      }
+    } else {
+      // Redirect to the login page
+      window.location.href = "/login";
+    }
+  };
 
-	// Use effect to check if the product is in favorites when the component mounts
-	useEffect(() => {
-		const checkFavoriteStatus = async () => {
-			try {
-				// Fetch user favorites
-				const favorites = await apiInstance.get(FAVORITE_ENDPOINT);
+  // Use effect to check if the product is in favorites when the component mounts
+  useEffect(() => {
+    const checkFavoriteStatus = async () => {
+      try {
+        // Fetch user favorites
+        const favorites = await apiInstance.get(FAVORITE_ENDPOINT);
 
-				// Check if the product is in favorites
-				const isProductInFavorites = favorites.data.some(
-					(favorite) => favorite.product === productId
-				);
+        // Check if the product is in favorites
+        const isProductInFavorites = favorites.data.some(
+          (favorite) => favorite.product === productId
+        );
 
-				setIsFavorite(isProductInFavorites);
-			} catch (error) {
-				console.error("Error checking favorite status:", error);
-			}
-		};
+        setIsFavorite(isProductInFavorites);
+      } catch (error) {
+        console.error("Error checking favorite status:", error);
+      }
+    };
 
-		// Call the function to check favorite status
-		checkFavoriteStatus();
-	}, [productId]); // Add productId as a dependency to re-run the effect when productId changes
+    // Call the function to check favorite status
+    checkFavoriteStatus();
+  }, [productId]); // Add productId as a dependency to re-run the effect when productId changes
 
-	return (
-		<div className="sp-details-hover">
-			{/* {stock === 0 ? (
+  return (
+    <div className="sp-details-hover">
+      {/* {stock === 0 ? (
 				userId &&
 				!userIdsToNotify.includes(userId) && (
 					<Link
@@ -92,35 +92,28 @@ const ProductAddToCart = ({
 					<span>Add to cart</span>
 				</Link>
 			)} */}
-			{stock === 0 ? (
-				userId ? (
-					userIdsToNotify.includes(userId) ? (
-						<span className="pro-badge2 out-of-stock">
-							Out of Stock
-						</span>
-					) : (
-						<Link
-							className="sp-cart"
-							onClick={handleAddProductToNotifications}
-						>
-							<span>Notify When Available</span>
-						</Link>
-					)
-				) : (
-					<span className="pro-badge2 out-of-stock">
-						Out of Stock
-					</span>
-				)
-			) : (
-				<Link
-					className="sp-cart"
-					onClick={() => handleAddProductToCart(productId)}
-				>
-					<i className="twi-cart-plus"></i>
-					<span>Add to cart</span>
-				</Link>
-			)}
-			{/* <Link
+      {stock === 0 ? (
+        userId ? (
+          userIdsToNotify.includes(userId) ? (
+            <span className="pro-badge2 out-of-stock">Out of Stock</span>
+          ) : (
+            <Link className="sp-cart" onClick={handleAddProductToNotifications}>
+              <span>Notify When Available</span>
+            </Link>
+          )
+        ) : (
+          <span className="pro-badge2 out-of-stock">Out of Stock</span>
+        )
+      ) : (
+        <Link
+          className="sp-cart"
+          onClick={() => handleAddProductToCart(productId)}
+        >
+          <i className="twi-cart-plus"></i>
+          <span>Add to cart</span>
+        </Link>
+      )}
+      {/* <Link
 				className="sp-cart"
 				onClick={() => handleAddProductToCart(productId)}
 			>
@@ -128,15 +121,15 @@ const ProductAddToCart = ({
 				<span>Add to cart</span>
 			</Link> */}
 
-			{/* Add to Wishlist */}
-			<Link
-				className={`sp-wishlist ${isFavorite ? "backgroundBlack" : ""}`}
-				onClick={handleAddToWishlist}
-			>
-				<i className="twi-heart2"></i>
-			</Link>
-		</div>
-	);
+      {/* Add to Wishlist */}
+      <Link
+        className={`sp-wishlist ${isFavorite ? "backgroundBlack" : ""}`}
+        onClick={handleAddToWishlist}
+      >
+        <i className="twi-heart2"></i>
+      </Link>
+    </div>
+  );
 };
 
 export default ProductAddToCart;
