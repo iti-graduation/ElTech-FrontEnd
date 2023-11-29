fa83a24f150c2d5d4d6ebe77b0671a12fb137177
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2023-11-30 00:19:18 +0200

    fixing the pichart in the dashboard

--- src/components/Dashboard/Home/HomePie.jsx
+++ src/components/Dashboard/Home/HomePie.jsx
@@ -1,33 +1,46 @@
-import * as React from 'react';
-import { PieChart,pieArcLabelClasses } from '@mui/x-charts/PieChart';
+import * as React from "react";
+import { PieChart, pieArcLabelClasses } from "@mui/x-charts/PieChart";
 
 export default function HomePie({ categoryProducts }) {
-    const filteredData = categoryProducts.filter(category => category.productsCount !== 0);
+  const filteredData = categoryProducts.filter(
+    (category) => category.productsCount !== 0
+  );
 
-
-    return (
-      <PieChart
+  return (
+    <PieChart
       series={[
         {
-          arcLabel: (item) => `${item.label} (${item.value})`,
-          arcLabelMinAngle: 45,
+          // arcLabel: (item) => ${item.label} (${item.value}),
+          // arcLabelMinAngle: 45,
           data: filteredData.map((category) => ({
             id: category.id,
             value: category.productsCount,
             label: category.name,
           })),
+          highlightScope: { faded: "global", highlighted: "item" },
+          faded: { innerRadius: 50, additionalRadius: -20, color: "black" },
         },
       ]}
-        sx={{
-            [`& .${pieArcLabelClasses.root}`]: {
-              fill: 'white',
-              fontWeight: 'bold',
-            },
-            marginTop:'',
-            marginLeft:'0',
-          }}
-        width={600}
-        height={300}
-      />
-    );
-  }
+      sx={
+        {
+          // [& .${pieArcLabelClasses.root}]: {
+          //   fill: 'white',
+          //   fontWeight: 'bold',
+          //   marginTop: '20px', // Increase top margin to create space
+          //   marginLeft: '20px',
+          //   overflow: 'hidden',
+          //   textOverflow: 'ellipsis',
+          //   whiteSpace: 'nowrap',
+          // },
+          // marginTop:'',
+          // marginLeft:'20px',
+          // width: '300',
+          // height: '150',
+        }
+      }
+      // width={700}
+      height={300}
+      margin={{ right: 350 }}
+    />
+  );
+}
